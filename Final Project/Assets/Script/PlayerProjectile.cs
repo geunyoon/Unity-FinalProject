f367e606138d808684b4dc42518ebfdef8e2734a
geunyoon <geunyoon7@gmail.com>
2019-04-24 00:16:28 -0400

    Enable Player Shooting

---
+++ Final Project/Assets/Script/PlayerProjectile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProjectile : MonoBehaviour
+{
+    public float damage;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name != "Player")
+        {
+            if(collision.GetComponent<EnemyRecieveDamage>() != null)
+            {
+                collision.GetComponent<EnemyRecieveDamage>().DealDamage(damage);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
