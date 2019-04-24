29ae020ae1ba05d17e9f19447b0f97c8b81d6745
geunyoon <geunyoon7@gmail.com>
2019-04-24 02:51:00 -0400

    apply dash mechanic

--- Final Project/Assets/Script/PlayerMovement.cs
+++ Final Project/Assets/Script/PlayerMovement.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float dashRange;
     public float speed;
+    private Vector2 targetPos;
     private Vector2 direction;
     private Animator animator;
+    private enum Facing { UP, DOWN, LEFT, RIGHT };
+    private Facing FacingDir = Facing.RIGHT;
 
+    
     private void Start()
     {
         animator = GetComponent<Animator>();
@@ -38,21 +43,47 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             direction += Vector2.up;
+            FacingDir = Facing.UP;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             direction += Vector2.down;
+            FacingDir = Facing.DOWN;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             direction += Vector2.left;
+            FacingDir = Facing.LEFT;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             direction += Vector2.right;
+            FacingDir = Facing.RIGHT;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector2 currentPos = transform.position;
+            targetPos = Vector2.zero;
+            if (FacingDir == Facing.UP)
+            {
+                targetPos.y = 1;
+            }
+            else if(FacingDir == Facing.DOWN)
+            {
+                targetPos.y = -1;
+            }
+            else if (FacingDir == Facing.LEFT)
+            {
+                targetPos.x = -1;
+            }
+            else if (FacingDir == Facing.RIGHT)
+            {
+                targetPos.x = 1;
+            }
+            transform.Translate(targetPos * dashRange);
         }
     }
 
