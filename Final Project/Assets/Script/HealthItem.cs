a50dc9be82f2288aa12734d5169bc3fc5ad66811
geunyoon <geunyoon7@gmail.com>
2019-05-01 08:46:09 -0400

    Able to grab, drop, click on the inventory item

---
+++ Final Project/Assets/Script/HealthItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthItem : MonoBehaviour
+{
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    public void Use()
+    {
+        Destroy(gameObject);
+    }
+}
