aa874b19288d12d8e1a59c78735e2b5013fb2805
geunyoon <geunyoon7@gmail.com>
2019-05-01 08:55:49 -0400

    Make enemy to follow the player

---
+++ Final Project/Assets/Script/EnemyFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public float speed;
+    private Transform target;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        if (Vector2.Distance(transform.position, target.position) > 2)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+}
